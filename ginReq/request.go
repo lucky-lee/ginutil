88f133afa3d3d010082f1f35deda5e6c9b30be07
李吉才 <lijicai0468@126.com>
2020-08-19 20:22:09 +0800

    fix

--- ginReq/request.go
+++ ginReq/request.go
@@ -2,17 +2,12 @@ package ginReq
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 //header auth get
 func HeaderAuth(c *gin.Context) string {
 	auth := c.Request.Header.Get("Authorization")
 	if auth == "" {
-		if !c.IsAborted() {
-			c.AbortWithStatusJSON(http.StatusForbidden, "err")
-		}
-
 		return ""
 	}
 	return auth
